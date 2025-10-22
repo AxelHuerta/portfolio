02ce041063243958f0658349583527a3a5ce5b89
Axel Huerta <axelhuerta.dev@gmail.com>
2025-10-22 17:35:28 -0600

    update styles

---
+++ src/lib/tags.ts
@@ -0,0 +1,98 @@
+import CSS3 from "../components/icons/CSS3.astro";
+import HTML from "../components/icons/HTML.astro";
+import Java from "../components/icons/Java.astro";
+import Javascript from "../components/icons/Javascript.astro";
+import Laravel from "../components/icons/Laravel.astro";
+import Nextjs from "../components/icons/Nextjs.astro";
+import Node from "../components/icons/Node.astro";
+import React from "../components/icons/React.astro";
+import SpringBoot from "../components/icons/SpringBoot.astro";
+import Typescript from "../components/icons/Typescript.astro";
+import CSS from "../components/icons/CSS.astro";
+import Express from "../components/icons/Express.astro";
+import MySQL from "../components/icons/MySQL.astro";
+import PostgreSQL from "../components/icons/PostgreSQL.astro";
+import Mongo from "../components/icons/Mongo.astro";
+import Git from "../components/icons/Git.astro";
+import AstroLogo from "../components/icons/AstroLogo.astro";
+import Docker from "../components/icons/Docker.astro";
+import Tailwind from "../components/icons/Tailwind.astro";
+
+export const TAGS = {
+  NEXTJS: {
+    name: "Next.js",
+    icon: Nextjs,
+  },
+  LARAVEL: {
+    name: "Laravel",
+    icon: Laravel,
+  },
+  REACT: {
+    name: "React",
+    icon: React,
+  },
+  TYPESCRIPT: {
+    name: "Typescript",
+    icon: Typescript,
+  },
+  NODE: {
+    name: "Node",
+    icon: Node,
+  },
+  EXPRESS: {
+    name: "Express",
+    icon: Express,
+  },
+  JAVASCRIPT: {
+    name: "Javascript",
+    icon: Javascript,
+  },
+  HTML: {
+    name: "HTML",
+    icon: HTML,
+  },
+  CSS3: {
+    name: "CSS3",
+    icon: CSS3,
+  },
+  CSS: {
+    name: "CSS",
+    icon: CSS,
+  },
+  JAVA: {
+    name: "Java",
+    icon: Java,
+  },
+  SPRINGBOOT: {
+    name: "Spring Boot",
+    icon: SpringBoot,
+  },
+  MYSQL: {
+    name: "MySQL",
+    icon: MySQL,
+  },
+  POSTGRESQL: {
+    name: "PostgreSQL",
+    icon: PostgreSQL,
+  },
+  MONGODB: {
+    name: "MongoDB",
+    icon: Mongo,
+  },
+  GIT: {
+    name: "Git",
+    icon: Git,
+  },
+  ASTRO: {
+    name: "Astro",
+    icon: AstroLogo,
+  },
+  DOCKER: {
+    name: "Docker",
+    icon: Docker,
+  },
+  TAILWINDCSS: {
+    name: "TailwindCSS",
+    icon: Tailwind,
+  },
+};
